9ee5d1282ff2d9d618e44ba9630c5cd516dfc28a
MetheuAA <matheo200817@gmail.com>
2024-04-17 09:57:31 -0400

    add questao8

---
+++ questao8.js
@@ -0,0 +1,7 @@
+/*Questão 8: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é divisível por 3. A variável numero contém o valor a 
+ser verificado. Atribua o resultado à variável resultado. */
+
+let numero = 6
+let resultado = (numero %3 == 0 ) ? "É divisivel" : "Não é divisivel";
+console.log (resultado)
