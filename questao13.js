632d50fa14e46f5a992ceddae38c211cedbfcb6d
MetheuAA <matheo200817@gmail.com>
2024-04-17 09:57:59 -0400

    add questao13

---
+++ questao13.js
@@ -0,0 +1,10 @@
+/*Questão 13: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se uma pessoa é elegível para a aposentadoria. A variável idade 
+contém a idade da pessoa e a variável sexo indica o sexo ( 'masculino' ou 
+'feminino'). Atribua o resultado à variável elegivel.*/
+
+let idade = 20;
+let sexo = "masculino"
+let resultado = (idade >= 60) ? " elegivel a aposentadoria" : "não elegivel"
+console.log (idade)
+console.log (resultado)
