b301662819bbfcfaadd692d27eccdd440e46d1cf
MetheuAA <matheo200817@gmail.com>
2024-04-17 09:57:48 -0400

    add questao11

---
+++ questao11.js
@@ -0,0 +1,7 @@
+/*Questão 11: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se uma pessoa é um bebê, criança, adolescente, adulto ou idoso. A 
+variável idade contém a idade da pessoa. Atribua o resultado à variável 
+categoria.*/
+let idade = 1
+let resultado = (idade <= 0 ) ? "Bebê" : "criança"
+console.log(resultado)
